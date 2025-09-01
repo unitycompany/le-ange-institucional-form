2cb4ddfa6441ea220d3335508081d4ea91c6dad0
Aleph Ramos <alephsramosdev@gmail.com>
2025-09-01 09:12:09 -0300

    commit rápido

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   build: {
-     rollupOptions: {
-       external: [
-          "/assets/index-BHhcukG4.js" 
-       ],
-     },
+    rollupOptions: {
+      external: [
+        "/assets/index-BHhcukG4.js"
+      ],
+    },
   },
   plugins: [react()],
   base: "/",
