0bcff0fe053307e39692e51ffe13baebc04eb167
Aleph Ramos <equipe@unitycompany.com.br>
2025-12-23 17:19:37 -0300

    commit rápido

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -615,7 +615,6 @@ const Footer = () => {
                 <StyledPolitica>
                     <p>Todos os direitos reservados | <RiCopyrightLine /> 2024</p>
                     <a href='https://pousadaleange.com.br/politica-de-privacidade'>Termos de condições</a>
-                    <AlephsramosdevWidget />
                 </StyledPolitica>
 
 
